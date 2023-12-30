4b750497110ad487de6ad29b470543129985f846
yy18biao <jingbiaoh@163.com>
2023-12-30 15:37:36 +0800

    Util类实现完毕

--- Util.hpp
+++ Util.hpp
@@ -2,6 +2,7 @@
 #define __M_UTIL_H__
 
 #include "Log.hpp"
+#include <fstream>
 #include <jsoncpp/json/json.h>
 #include <mysql/mysql.h>
 #include <string>
@@ -65,7 +66,7 @@ public:
     }
 };
 
-class JSON
+class JSON_Uitl
 {
 public:
     // 序列化
@@ -100,6 +101,69 @@ public:
             ERR_LOG("反序列化失败");
             return false;
         }
+        return true;
+    }
+};
+
+class String_Uitl
+{
+public:
+    // 分割字符串
+    static int split(const std::string &str, const std::string &sep, std::vector<std::string> &res)
+    {
+        size_t pos, idx = 0;
+        while (idx < str.size())
+        {
+            pos = str.find(sep, idx);
+            if (pos == std::string::npos)
+            {
+                res.push_back(str.substr(idx));
+                break;
+            }
+            if (pos == idx)
+            {
+                idx += sep.size();
+                continue;
+            }
+            res.push_back(str.substr(idx, pos));
+            idx = pos + sep.size();
+        }
+        return res.size();
+    }
+};
+
+class File_Util
+{
+public:
+    // 读取文件所有内容
+    static bool read(const std::string &filename, std::string &body)
+    {
+        // 打开文件
+        std::ifstream ifs(filename, std::ios::binary);
+        if (!ifs.is_open())
+        {
+            ERR_LOG("打开 %s 此文件错误", filename.c_str());
+            return false;
+        }
+
+        // 获取文件大小
+        size_t fsize = 0;
+        ifs.seekg(0, std::ios::end);
+        fsize = ifs.tellg();
+        ifs.seekg(0, std::ios::beg);
+        body.resize(fsize);
+
+        // 读取数据
+        ifs.read(&body[0], fsize);
+        if (!ifs.good())
+        {
+            ERR_LOG("读取 %s 此文件错误", filename.c_str());
+            ifs.close();
+            return false;
+        }
+
+        // 关闭文件
+        ifs.close();
         return true;
     }
 };
